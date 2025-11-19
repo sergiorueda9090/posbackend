9fc3d911ca0435f839311155e2cc8e9ca90cc3f4
sergiorueda9090 <sergiorueda90@hotmail.com>
2025-11-18 22:20:41 -0500

    cambios

--- backend/settings.py
+++ backend/settings.py
@@ -23,7 +23,7 @@ load_dotenv()
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 #usa el dotenv para cargar las variables de entorno
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-kja8h231kjh12lk3jh1k23jh1k23jhk')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'True') == 'True'
